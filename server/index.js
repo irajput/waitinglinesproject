4f56da753b8e04a30bfb0ace3a4c06160119cb22
unknown <ishar0643@gmail.com>
2021-11-14 18:45:41 -0800

    added connection to the database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // server/index.js
 
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,6 +11,24 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log("Connected to database");
+  } catch (err) {
+    console.log(err);
+    console.log("Could not connect to database. Exiting...");
+    process.exit(1);
+  }
+};
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+connectToDB();
