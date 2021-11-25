fb82fc8a96346839268dbbe7f02563e0471d543f
Krisha <kminocha4@gmail.com>
2021-11-24 23:23:53 -0800

    Added login functionality to front-end

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -2,13 +2,24 @@
 
 import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Dashboard from './Dashboard.js';
-import Preferences from './Preferences.js';
-import Login from './Login.js';
+import Dashboard from '../Dashboard.js';
+import Preferences from '../Preferences.js';
+import Login from '../Login/Login';
 import "./App.css";
+import useToken from './useToken';
+
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
 
 function App() {
-  const [token, setToken] = useState();
+  const { token, setToken } = useToken();
 
   if(!token) {
     return <Login setToken={setToken} />
