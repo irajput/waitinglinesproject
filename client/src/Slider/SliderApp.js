d6ce004d8da626c0ec4f0df84d42cc1c62d69481
Vandita Sethuraman <vandita.sethuraman@gmail.com>
2021-11-26 20:35:55 -0800

    changed to dropdown menu, store crowd state and choice of dining hall

--- client/src/Slider/SliderApp.js
+++ client/src/Slider/SliderApp.js
@@ -1,8 +1,13 @@
 import { useState } from "react";
+import React from "react";
+import ReactSlider from "react-slider";
+
 
 import Step from "./Step";
 import Slider from "./SliderInd/sliderIndex.js";
 import "./styles.css";
+
+{/*
 
 export default function App() {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -10,14 +15,8 @@ export default function App() {
   const _handleIndexChange = (index) => {
     setCurrentIndex(index);
     console.log("index:" , index)
-    
-  };
 
-  const _handleNext = (currentIndex) => {
-    setCurrentIndex(currentIndex + 1);
   };
-
-  const _handleComplete = () => {};
 
   return (
     <div className="App">
@@ -25,6 +24,81 @@ export default function App() {
         <Step currentIndex={currentIndex} />
       </div>
       <Slider onChange={_handleIndexChange} currentIndex={currentIndex} />
+
+      <h1 className="whichDH"> Which Dining Hall: </h1>
+      <select className="dropdown">
+        <option selected value = "DeNeve">De Neve</option>
+        <option value = "Rende">Rendezvous</option>
+      </select>
     </div>
+
+
   );
 }
+*/}
+
+class HallForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'De Neve',
+      choice: '1',
+    };
+
+    this.handleChoice = this.handleChoice.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleChange(event) {
+      this.setState({value: event.target.value});
+  }
+
+  handleChoice(event) {
+    this.setState({choice: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('You submitted data for: ' + this.state.value + " val: " + this.state.choice);
+    event.preventDefault();
+
+  }
+
+  render() {
+    return (
+
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Which Dining Hall:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="DeNeve">De Neve</option>
+            <option value="Rendezvous">Rendezvous</option>
+            <option value="Study">The Study</option>
+            <option value="Feast">Feast</option>
+            <option value="BCafe">BCafe</option>
+            <option value="Epic">Epicuria</option>
+            <option value="Bplate">Bplate</option>
+          </select>
+        </label>
+
+        <label>
+          How Crowded Is It:
+          <select value={this.state.choice} onChange={this.handleChoice}>
+            <option value="1">Not Busy</option>
+            <option value="2">Slightly Busy</option>
+            <option value="3">Moderately Busy</option>
+            <option value="4">Busy</option>
+            <option value="5">Extremely Busy</option>
+
+          </select>
+        </label>
+
+        <input type="submit" value="Submit" />
+      </form>
+
+    );
+  }
+}
+
+export default HallForm
