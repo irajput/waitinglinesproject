89a1ec94fe5cec5393c97d3c3482a73ef4ea9791
lindh173 <lindh173@umn.edu>
2021-11-28 13:20:06 -0800

    began working on function for posting profile changes

--- client/src/Profile/Profile.js
+++ client/src/Profile/Profile.js
@@ -32,19 +32,29 @@ async function profile() {
   }
 
  class Profile extends Component {
-  // constructor(props) {
-  //     super(props);
+  constructor(props) {
+      super(props);
+
+      this.postProfileChanges = this.postProfileChanges.bind(this);
   
-  //     // this.state = {
-  //     //   
-  //     // } 
-  // }
+      // this.state = {
+      //   
+      // } 
+  }
+
+  postProfileChanges() {
+    console.log("posting profile changes");
+    var maxWaitInput = document.getElementById("wait-min");
+    var maxWait = maxWaitInput.value;
+    
+  }
 
   render() {
     return (
       <div className="profile">
         <input type="button" value="Log profile" onClick={logProfile}/>
         <h1>Profile Page</h1>
+        <input type="button" value="Submit Changes" onClick={this.postProfileChanges}/>
         <h3>Receive notifications</h3>
         <Switch />
         <div id="container">
@@ -54,16 +64,32 @@ async function profile() {
             <input type="number" id="wait-min" name="wait-min" min="1" max="60" />
             <h3>Receive notifications for these halls:</h3>
             <div id="outer">
-                <Checkbox className = "inner"/>
+                <Checkbox className = "inner" id = "0"/>
+                <p className = "inner"> Hall 0 </p>
+            </div>
+            <div id="outer">
+                <Checkbox className = "inner" id = "1"/>
                 <p className = "inner"> Hall 1 </p>
             </div>
             <div id="outer">
-                <Checkbox className = "inner"/>
+                <Checkbox className = "inner" id = "2"/>
                 <p className = "inner"> Hall 2 </p>
             </div>
             <div id="outer">
-                <Checkbox className = "inner"/>
+                <Checkbox className = "inner" id = "3"/>
                 <p className = "inner"> Hall 3 </p>
+            </div>
+            <div id="outer">
+                <Checkbox className = "inner" id = "4"/>
+                <p className = "inner"> Hall 4 </p>
+            </div>
+            <div id="outer">
+                <Checkbox className = "inner" id = "5"/>
+                <p className = "inner"> Hall 5 </p>
+            </div>
+            <div id="outer">
+                <Checkbox className = "inner" id = "6"/>
+                <p className = "inner"> Hall 6 </p>
             </div>
           </div>
         </div>
