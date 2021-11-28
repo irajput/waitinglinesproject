05ea87061678cf3e50e958db1afd8dfd5c890e92
Alex Walburg <ajwalburg@g.ucla.edu>
2021-11-28 14:37:19 -0800

    Set cors headers to allow requests from everywhere

--- server/index.js
+++ server/index.js
@@ -127,7 +127,12 @@ passport.use(
     )
 );
 
- 
+app.use((req,res,next) =>{
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+	next();
+});
 
 //////////////////
 // Routing code //
