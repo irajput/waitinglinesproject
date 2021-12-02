caaa2c542f472b54d87920df9a1bffaab2556aa4
Krisha <kminocha4@gmail.com>
2021-12-01 19:17:47 -0800

    Changed y-axis to minutes not seconds

--- client/src/History/HChart.js
+++ client/src/History/HChart.js
@@ -23,6 +23,9 @@ function HChart() {
         //console.log(x[0]); 
         let w = x[0]/60000;
         let m = w%60;
+        if (m%10 == 0) {
+            m = m + "0";
+        }
         let h = Math.floor(w/60);
         let s = h + ":" + m;
         return s;
